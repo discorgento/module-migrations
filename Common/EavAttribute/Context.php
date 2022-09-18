ca0f890ac108a4e9f976be75bdf26a14e9dd0204
Code Slicer <ivanaugustobd@pm.me>
2022-09-18 11:37:54 -0300

    feat: greatly simplify product attribute set assignment

--- Common/EavAttribute/Context.php
+++ Common/EavAttribute/Context.php
@@ -4,14 +4,18 @@
 namespace Discorgento\Migrations\Common\EavAttribute;
 
 use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\App\ResourceConnection;
 
 final class Context
 {
     public EavSetupFactory $eavSetupFactory;
+    public ResourceConnection $resourceConnection;
 
     public function __construct(
-        EavSetupFactory $eavSetupFactory
+        EavSetupFactory $eavSetupFactory,
+        ResourceConnection $resourceConnection
     ) {
         $this->eavSetupFactory = $eavSetupFactory;
+        $this->resourceConnection = $resourceConnection;
     }
 }
